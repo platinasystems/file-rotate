ae342f0bc7bc99714dda2dfdd8c7643b6a1f2325
mar86.cvl@gmail.com <mar86.cvl@gmail.com>
2023-05-09 11:15:59 +0200

    make filename configurable change rotation add writer counter

--- rotate.go
+++ rotate.go
@@ -3,7 +3,7 @@
 // It will also only keep a fixed number of files.
 // It can be used anywhere an io.Writer is used, for example in
 // log.SetOutput().
-package rotate // import "stathat.com/c/rotate"
+package rotate
 
 import (
 	"errors"
@@ -11,14 +11,15 @@ import (
 	"os"
 	"path"
 	"sort"
+	"strconv"
 	"strings"
 	"sync"
-	"time"
 )
 
 const (
 	maxDefault  = 1024 * 1024 * 8
 	keepDefault = 10
+	fileDefault = "default.log"
 )
 
 // RootPerm defines the permissions that Writer will use if it
@@ -33,12 +34,14 @@ var FilePerm = os.FileMode(0666)
 // "current" file in the root directory.  When current's size
 // exceeds max, it is renamed and a new file is created.
 type Writer struct {
-	root    string
-	prefix  string
-	current *os.File
-	size    int
-	max     int
-	keep    int
+	root     string
+	prefix   string
+	fileName string
+	current  *os.File
+	size     int
+	max      int
+	keep     int
+	counter  int
 	sync.Mutex
 }
 
@@ -46,7 +49,7 @@ type Writer struct {
 // root directory.  root will be created if necessary.  The
 // filenames will start with prefix.
 func New(root, prefix string) (*Writer, error) {
-	l := &Writer{root: root, prefix: prefix, max: maxDefault, keep: keepDefault}
+	l := &Writer{root: root, prefix: prefix, fileName: fileDefault, max: maxDefault, keep: keepDefault, counter: 1}
 	if err := l.setup(); err != nil {
 		return nil, err
 	}
@@ -58,9 +61,29 @@ func (r *Writer) SetMax(size int) {
 	r.max = size
 }
 
+// SetMaxMiB sets the maximum size for a file in Mebibyte.
+func (r *Writer) SetMaxMiB(size int) {
+	r.max = size * 1024 * 1024
+}
+
+// SetFileName sets the file name.
+func (r *Writer) SetFileName(name string) {
+	r.fileName = name
+}
+
 // SetKeep sets the number of archived files to keep.
 func (r *Writer) SetKeep(n int) {
 	r.keep = n
+}
+
+// SetCounter sets the starting writer counter.
+func (r *Writer) SetCounter(c int) {
+	r.counter = c
+}
+
+// GetCounter return current counter.
+func (r *Writer) GetCounter() int {
+	return r.counter
 }
 
 // Write writes p to the current file, then checks to see if
@@ -114,7 +137,7 @@ func (r *Writer) setup() error {
 }
 
 func (r *Writer) openCurrent() error {
-	cp := path.Join(r.root, "current")
+	cp := path.Join(r.root, r.fileName)
 	var err error
 	r.current, err = os.OpenFile(cp, os.O_RDWR|os.O_CREATE|os.O_APPEND, FilePerm)
 	if err != nil {
@@ -128,13 +151,14 @@ func (r *Writer) rotate() error {
 	if err := r.current.Close(); err != nil {
 		return err
 	}
-	filename := fmt.Sprintf("%s_%d", r.prefix, time.Now().UnixNano())
-	if err := os.Rename(path.Join(r.root, "current"), path.Join(r.root, filename)); err != nil {
+	filename := fmt.Sprintf("%s_%d", r.prefix, r.counter)
+	if err := os.Rename(path.Join(r.root, r.fileName), path.Join(r.root, filename)); err != nil {
 		return err
 	}
 	if err := r.clean(); err != nil {
 		return err
 	}
+	r.counter = r.counter + 1
 	return r.openCurrent()
 }
 
@@ -143,6 +167,7 @@ func (r *Writer) clean() error {
 	if err != nil {
 		return err
 	}
+	defer d.Close()
 	names, err := d.Readdirnames(1024)
 	if err != nil {
 		return err
@@ -157,7 +182,14 @@ func (r *Writer) clean() error {
 		return nil
 	}
 
-	sort.Strings(archNames)
+	sort.Slice(archNames, func(i, j int) bool {
+		si := strings.Split(archNames[i], "_")
+		sj := strings.Split(archNames[j], "_")
+		ii, _ := strconv.Atoi(si[1])
+		jj, _ := strconv.Atoi(sj[1])
+		return ii < jj
+	})
+
 	toDel := archNames[0 : len(archNames)-r.keep]
 	for _, n := range toDel {
 		if err := os.Remove(path.Join(r.root, n)); err != nil {
